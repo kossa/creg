a9df1fe2b2ff74dcc5e219d354b5cd4669a6c5ea
Kouceyla <hadjikouceyla@gmail.com>
2021-02-18 11:00:05 +0100

    Installing breeze auth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,7 +25,7 @@
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                     @auth
-                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>
+                        <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 underline">Dashboard</a>
                     @else
                         <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
 
@@ -36,7 +36,6 @@
                 </div>
             @endif
 
-            <h1>Hello</h1>
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,18 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 
-// Route::get('/', 'PageController@home');
+Route::get('/', function () {
+    return view('welcome');
+});
 
-// Route::get('contact/{name?}', 'PageController@contact')->where('name', '[a-zA-Z]+');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-// Route::get('calcule/{name}/{year}', 'PageController@calcul')->name('calculate-age');
+require __DIR__.'/auth.php';
+
+Route::get('/', 'PageController@home');
 
 Route::get('users/{id}/delete', 'UserController@destroy');
 Route::resource('users', 'UserController');
